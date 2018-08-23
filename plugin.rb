b32f563bda26bc421e8089a14a86a5c2576f4b57
Muhlis <muhlisbc@gmail.com>
2018-08-23 13:36:06 +0700

    FIX: hide signup modal

--- plugin.rb
+++ plugin.rb
@@ -1,9 +1,11 @@
 # name: saml-auto-login
-# version: 0.1
+# version: 0.1.1
 # author: Procourse Team
 # url: https://github.com/procourse/discourse-saml-auto-login-plugin
 
 enabled_site_setting :saml_auto_login_enabled
+
+register_asset "stylesheets/saml-auto-login.scss"
 
 after_initialize {
 
